a5e07d892cd50b8d6c51ddb4b8f9ef5a7128d491
Nikunj <nikunjsavaliyan@gmail.com>
2019-03-16 11:30:02 -0700

    Commit_16/3/2019

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+mixins = require('postcss-mixins'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+
+
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(error){
+    	console.log(error.toString());
+    	this.emit('end')
+
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
